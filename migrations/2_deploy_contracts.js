78472f40a19d15a387dc9aa0abc2125ad1549f83
Simeon Taskaris <simos.taskaris@gmail.com>
2018-10-09 00:07:07 +0300

    20181009

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Betme = artifacts.require("./Betme.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = function (deployer) {
+    deployer.deploy(SimpleStorage);
+    deployer.deploy(Betme);
 };

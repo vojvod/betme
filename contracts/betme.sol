635956497ab4427e4f40b222b951b58e376fcf3b
vojvod <task@z40>
2018-10-08 16:23:38 +0300

    20181008

--- contracts/betme.sol
+++ contracts/betme.sol
@@ -10,10 +10,10 @@ contract Betme
         string _theBet;
         uint _amount;
         address _betWitness;
-        address _betparticipantOne;
-        uint _betparticipantOneAcceptBet;
-        address _betparticipantTwo;
-        uint _betparticipantTwoAcceptBet;
+        address _betParticipantOne;
+        uint _betParticipantOneAcceptBet;
+        address _betParticipantTwo;
+        uint _betParticipantTwoAcceptBet;
         uint _theWinner;
     }
 
@@ -25,8 +25,8 @@ contract Betme
     }
 
     modifier validParticipant(bytes32 betID, address addr, uint amount) {
-        require(_bets[betID]._betparticipantOne == addr || _bets[betID]._betparticipantTwo == addr);
-        require(msg.sender == _bets[betID]._betparticipantOne || msg.sender == _bets[betID]._betparticipantTwo);
+        require(_bets[betID]._betParticipantOne == addr || _bets[betID]._betParticipantTwo == addr);
+        require(msg.sender == _bets[betID]._betParticipantOne || msg.sender == _bets[betID]._betParticipantTwo);
         require(msg.value == amount);
         _;
     }
@@ -34,14 +34,14 @@ contract Betme
     modifier validWinner(address betWitness, bytes32 betID, uint wn) {
         require(_bets[betID]._betWitness == betWitness);
         require(_bets[betID]._theWinner == 0);
-        require(_bets[betID]._betparticipantOneAcceptBet == 1);
-        require(_bets[betID]._betparticipantTwoAcceptBet == 1);
+        require(_bets[betID]._betParticipantOneAcceptBet == 1);
+        require(_bets[betID]._betParticipantTwoAcceptBet == 1);
         require(wn == 1 || wn == 2);
         _;
     }
 
     modifier validParticipants(bytes32 betID, address betParticipant){
-        require(_bets[betID]._betWitness == betParticipant || _bets[betID]._betparticipantOne == betParticipant || _bets[betID]._betparticipantTwo == betParticipant);
+        require(_bets[betID]._betWitness == betParticipant || _bets[betID]._betParticipantOne == betParticipant || _bets[betID]._betParticipantTwo == betParticipant);
         _;
     }
 
@@ -53,7 +53,7 @@ contract Betme
 
     function createBet(string theBet, uint amount, address betWitness, address betparticipantOne, address betparticipantTwo)
     validValue
-    public payable returns(bytes32 betID) {
+    public payable returns (bytes32 betID) {
         bytes32 unique = keccak256(abi.encodePacked(nonce++, theBet));
         _bets[unique] = BetDetails(block.timestamp, theBet, amount, betWitness, betparticipantOne, 0, betparticipantTwo, 0, 0);
         _owner.transfer(msg.value);
@@ -63,12 +63,12 @@ contract Betme
     function acceptBet(bytes32 betID)
     validParticipant(betID, msg.sender, msg.value)
     public payable {
-        if(_bets[betID]._betparticipantOne == msg.sender){
-            _bets[betID]._betparticipantOneAcceptBet = 1;
+        if (_bets[betID]._betParticipantOne == msg.sender) {
+            _bets[betID]._betParticipantOneAcceptBet = 1;
             address(this).transfer(msg.value);
         }
-        else if(_bets[betID]._betparticipantTwo == msg.sender){
-            _bets[betID]._betparticipantTwoAcceptBet = 1;
+        else if (_bets[betID]._betParticipantTwo == msg.sender) {
+            _bets[betID]._betParticipantTwoAcceptBet = 1;
             address(this).transfer(msg.value);
         }
     }
@@ -90,19 +90,19 @@ contract Betme
         betDetails._theBet,
         betDetails._amount,
         betDetails._betWitness,
-        betDetails._betparticipantOne,
-        betDetails._betparticipantTwo,
+        betDetails._betParticipantOne,
+        betDetails._betParticipantTwo,
         betDetails._theWinner
         );
     }
 
     function setWinner(bytes32 betID, uint wn) validWinner(msg.sender, betID, wn) public {
         _bets[betID]._theWinner = wn;
-        if(wn == 1){
-            _bets[betID]._betparticipantOne.transfer(_bets[betID]._amount);
+        if (wn == 1) {
+            _bets[betID]._betParticipantOne.transfer(_bets[betID]._amount);
         }
-        else if(wn == 2){
-            _bets[betID]._betparticipantTwo.transfer(_bets[betID]._amount);
+        else if (wn == 2) {
+            _bets[betID]._betParticipantTwo.transfer(_bets[betID]._amount);
         }
     }
 

56f21cc78f8aa13b4c8455da8d208f68d05900d2
vojvod <task@z40>
2018-10-08 16:20:28 +0300

    20181008

--- contracts/betme.sol
+++ contracts/betme.sol
@@ -11,7 +11,9 @@ contract Betme
         uint _amount;
         address _betWitness;
         address _betparticipantOne;
+        uint _betparticipantOneAcceptBet;
         address _betparticipantTwo;
+        uint _betparticipantTwoAcceptBet;
         uint _theWinner;
     }
 
@@ -22,9 +24,18 @@ contract Betme
         _;
     }
 
-    modifier validWitness(address betWitness, bytes32 betID, uint wn) {
+    modifier validParticipant(bytes32 betID, address addr, uint amount) {
+        require(_bets[betID]._betparticipantOne == addr || _bets[betID]._betparticipantTwo == addr);
+        require(msg.sender == _bets[betID]._betparticipantOne || msg.sender == _bets[betID]._betparticipantTwo);
+        require(msg.value == amount);
+        _;
+    }
+
+    modifier validWinner(address betWitness, bytes32 betID, uint wn) {
         require(_bets[betID]._betWitness == betWitness);
         require(_bets[betID]._theWinner == 0);
+        require(_bets[betID]._betparticipantOneAcceptBet == 1);
+        require(_bets[betID]._betparticipantTwoAcceptBet == 1);
         require(wn == 1 || wn == 2);
         _;
     }
@@ -40,15 +51,31 @@ contract Betme
 
     function() public payable {}
 
-    function createBet(string theBet, uint amount, address betWitness, address betparticipantOne, address betparticipantTwo) validValue public payable returns(bytes32 betID) {
+    function createBet(string theBet, uint amount, address betWitness, address betparticipantOne, address betparticipantTwo)
+    validValue
+    public payable returns(bytes32 betID) {
         bytes32 unique = keccak256(abi.encodePacked(nonce++, theBet));
-        _bets[unique] = BetDetails(block.timestamp, theBet, amount, betWitness, betparticipantOne, betparticipantTwo, 0);
+        _bets[unique] = BetDetails(block.timestamp, theBet, amount, betWitness, betparticipantOne, 0, betparticipantTwo, 0, 0);
         _owner.transfer(msg.value);
-        address(this).transfer(amount);
         return (unique);
     }
 
-    function getBet(bytes32 betID, address betParticipant) validParticipants(betID, betParticipant) public constant returns (
+    function acceptBet(bytes32 betID)
+    validParticipant(betID, msg.sender, msg.value)
+    public payable {
+        if(_bets[betID]._betparticipantOne == msg.sender){
+            _bets[betID]._betparticipantOneAcceptBet = 1;
+            address(this).transfer(msg.value);
+        }
+        else if(_bets[betID]._betparticipantTwo == msg.sender){
+            _bets[betID]._betparticipantTwoAcceptBet = 1;
+            address(this).transfer(msg.value);
+        }
+    }
+
+    function getBet(bytes32 betID, address betParticipant)
+    validParticipants(betID, betParticipant)
+    public constant returns (
         uint timestamp,
         string theBet,
         uint amount,
@@ -69,7 +96,7 @@ contract Betme
         );
     }
 
-    function setWinner(bytes32 betID, uint wn) validWitness(msg.sender, betID, wn) public {
+    function setWinner(bytes32 betID, uint wn) validWinner(msg.sender, betID, wn) public {
         _bets[betID]._theWinner = wn;
         if(wn == 1){
             _bets[betID]._betparticipantOne.transfer(_bets[betID]._amount);
